9025006d53829d089d609b3737383575438140ec
JpScoot <jamespjones0714@gmail.com>
2022-09-06 10:56:42 -0700

    Added verifyInternetConnection.

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,14 +1,50 @@
 public class Television {
-  //instant variables or fields
-  //type name
-  String brand;
-  int volume;
 
-  void turnOn(){
+  //declare instant variables or fields
+  //type name
+  //private
+  private String brand;
+  private int volume;
+
+//write business methods
+//public modifiers
+  public void turnOn(){
+    //boolean variable = (assign a value)
+    boolean isConnected = verifyInternetConnection();
     System.out.println("The " + brand + " television is on with a volume of " + volume);
+
   }
-  void turnOff(){
+  public void turnOff(){
     System.out.println("The " + brand + " television is off! ");
   }
 
+  public String getBrand() {
+    return brand;
+  }
+//Accessor Methods
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public int getVolume() {
+    return volume;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  //helper method always private
+  private boolean verifyInternetConnection(){
+    return true;
+  }
+
+  //toString()
+  @Override
+  public String toString() {
+    return "Television{" +
+        "brand='" + brand + '\'' +
+        ", volume=" + volume +
+        '}';
+  }
 }
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -1,19 +1,25 @@
 public class TelevisionClient {
 //main class
+//main method
   public static void main(String[] args) {
+
+    //create instances(objects) of Television class getters and setters
     Television tv1 = new Television();
-    tv1.brand = "Samsung";
-    tv1.volume = 20;
+    tv1.setBrand("Samsung");
+    tv1.setVolume(20);
 
     Television tv2 = new Television();
-    tv2.brand = "Sony";
-    tv2.volume = 50;
+    tv2.setBrand("Sony");
+    tv2.setVolume(50);
 
     tv1.turnOn();
     tv1.turnOff();
 
     tv2.turnOn();
     tv2.turnOff();
+
+    System.out.println(tv1.toString());
+    System.out.println(tv1);
   }
 
 }
